327ac2e59419ea1915ca6672a730f21fb8729a5f
Julian Kent <jkflying@gmail.com>
2019-07-17 17:53:23 +0200

    Move the specific lib dependencies to the test, instead of in all gtests

--- cmake/gtest/px4_add_gtest.cmake
+++ cmake/gtest/px4_add_gtest.cmake
@@ -59,22 +59,7 @@ function(px4_add_gtest)
 		add_executable(${TESTNAME} EXCLUDE_FROM_ALL ${SRC})
 
 		# link the libary to test and gtest
-		target_link_libraries(${TESTNAME} ${LINKLIBS} gtest_main
-		                                              px4_daemon
-		                                              px4_platform
-		                                              modules__uORB
-		                                              px4_layer
-		                                              systemlib
-		                                              cdev
-		                                              px4_work_queue
-		                                              px4_daemon
-		                                              work_queue
-		                                              parameters
-		                                              perf
-		                                              tinybson
-		                                              uorb_msgs
-		                                              test_stubs #put this last
-		                                              )
+		target_link_libraries(${TESTNAME} ${LINKLIBS} gtest_main)
 
 		# add the test to the ctest plan
 		add_test(NAME ${TESTNAME} COMMAND ${TESTNAME})
