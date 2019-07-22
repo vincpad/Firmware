926ad535110610a5fdf5b49c6914f5e09924e79f
Julian Kent <jkflying@gmail.com>
2019-07-22 12:56:59 +0200

    Remove duplicate symbols

--- src/platforms/common/test_stubs/stub_parameter.cpp
+++ src/platforms/common/test_stubs/stub_parameter.cpp
@@ -3,20 +3,6 @@
 
 
 extern "C" {
-	int work_queue(int qid, struct work_s *work, worker_t worker, void *arg, uint32_t delay)
-	{
-		return stub_work_queue_callback(qid, work, worker, arg, delay);
-	}
-
-	int work_cancel(int qid, struct work_s *work)
-	{
-		return stub_work_cancel_callback(qid, work);
-	}
-
-	int px4_shutdown_lock() {return stub_px4_shutdown_lock_callback();}
-
-	int px4_shutdown_unlock() {return stub_px4_shutdown_unlock_callback();}
-
 	/* This function blocks forever in tests, so override it with a version that can be customized */
 	int pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex)
 	{
--- src/platforms/common/test_stubs/stub_parameter.h
+++ src/platforms/common/test_stubs/stub_parameter.h
@@ -1,16 +1,8 @@
 #pragma once
 
-#include "px4_workqueue.h"
+#include <pthread.h>
 
 #include <functional>
-#include <sys/types.h>
-#include <stdint.h>
 
-
-std::function<int(int, work_s *, worker_t, void *, uint32_t)> stub_work_queue_callback =
-[](int, work_s *, worker_t, void *, uint32_t) {return 0;};
-std::function<int(int, work_s *)> stub_work_cancel_callback = [](int, work_s *) {return 0;};
-std::function<int(void)> stub_px4_shutdown_lock_callback = []() {return 0;};
-std::function<int(void)> stub_px4_shutdown_unlock_callback = []() {return 0;};
 std::function<int(pthread_cond_t *, pthread_mutex_t *)> stub_pthread_cond_wait_callback =
 [](pthread_cond_t *, pthread_mutex_t *) {return 0;};
